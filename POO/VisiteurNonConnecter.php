20fc5f999391bf22c917aa654bc9eaa96f095c7e
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2025-12-25 23:03:15 +0100

    feat : ajoute la class de l'iscription avec la methode inscirire

--- POO/VisiteurNonConnecter.php
+++ POO/VisiteurNonConnecter.php
@@ -1,19 +1,39 @@
 <?php
 require_once "utilisateur.php";
+
 class VisiteurNonConnecter extends utilisateur 
 {
-    public function inscirire()
+    public function inscirire() : bool
     {
-    $obj= new Connexion();
-    $conn= $obj->connect();
-    if($this->getRoleUtilisateur() == "guide")
-    {
-        $sql = "INSERT INTO `utilisateurs`(`nom_utilisateur`,`email`,`role`,`motpasse_hash`,`Approuver_utilisateur`,`statut_utilisateur`) VALUES (:nom,:email,:role,:motpase,0,:status";
-    }
+        $obj = new Connexion();
+        $conn = $obj->connect();
 
-    else
-    {
-        $sql = "INSERT INTO `utilisateurs`(`nom_utilisateur`,`email`,`role`,`motpasse_hash`,`Approuver_utilisateur`,`statut_utilisateur`) VALUES (:nom,:email,:role,:motpase,1,:status";
-    }
+        if($this->getRoleUtilisateur() === "guide")
+        {
+     
+            $sql = "INSERT INTO `utilisateurs`(`nom_utilisateur`,`email`,`role`,`motpasse_hash`,`Approuver_utilisateur`) 
+                    VALUES (:nom, :email, 'guide', :motPase, 0)";
+        }
+        elseif($this->getRoleUtilisateur() === "visiteur")
+        {
+           
+            $sql = "INSERT INTO `utilisateurs`(`nom_utilisateur`,`email`,`role`,`motpasse_hash`) 
+                    VALUES (:nom, :email, 'visiteur', :motPase)";
+        }
+        else {
+            return false;
+        }
+
+        try {
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':nom', $this->nom_utilisateur);
+            $stmt->bindParam(':email', $this->email);
+            $stmt->bindValue(':motPase', password_hash($this->mot_passe, PASSWORD_BCRYPT));
+
+            return $stmt->execute();
+        } 
+        catch (Exception $e) {
+            return false;
+        }
     }
 }
