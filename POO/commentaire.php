4db9f7cda9fe5955b111096b68b095b630490000
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2025-12-26 11:28:06 +0100

    feat : ajoute la class de la commentaire pour grer les commentaire

---
+++ POO/commentaire.php
@@ -0,0 +1,204 @@
+
+<?php
+require_once 'Connexion.php';
+class Commentaire
+{
+    private int $id_commentaire;
+    private string $contenu_commentaire;
+    private DateTime $date_commentaire;
+    private int $note;
+    private int $id_visiteur;
+    private int $id_visite;
+
+    public function __construct() {}
+
+    public function getIdCommentaire(): int
+    {
+        return $this->id_commentaire;
+    }
+    public function getContenuCommentaire(): string
+    {
+        return $this->contenu_commentaire;
+    }
+    public function getDateCommentaire(): DateTime
+    {
+        return $this->date_commentaire;
+    }
+
+    public function getIdVisiteur(): int
+    {
+        return $this->id_visiteur;
+    }
+    public function getIdVisite(): int
+    {
+        return $this->id_visite;
+    }
+    public function getNote(): int
+    {
+        return $this->note;
+    }
+
+    public function setContenuCommentaire(string $contenu_commentaire)
+    {
+        if (strlen($contenu_commentaire) > 4 && strlen($contenu_commentaire) <= 500) {
+            $this->contenu_commentaire = $contenu_commentaire;
+            return true;
+        }
+        return false;
+    }
+  
+    public function setNote(int $note)
+    {
+        if ($note >= 1 && $note <= 5) {
+            $this->note = $note;
+            return true;
+        }
+        return false;
+    }
+    public function setIdVisiteur(int $id_visiteur)
+    {
+        if ($id_visiteur > 0) {
+            $this->id_visiteur = $id_visiteur;
+            return true;
+        }
+        return false;
+    }
+    public function setIdVisite(int $id_visite)
+    {
+        if ($id_visite > 0) {
+            $this->id_visite = $id_visite;
+            return true;
+        }
+        return false;
+    }
+    public function setIdCommentaire(int $id_commentaire)
+    {
+        if ($id_commentaire > 0) {
+            $this->id_commentaire = $id_commentaire;
+            return true;
+        }
+        return false;
+    }
+    public function __toString()
+    {
+        return "Commentaire ID: " . $this->id_commentaire . "\n" .
+            "Contenu: " . $this->contenu_commentaire . "\n" .
+            "Date: " . $this->date_commentaire->format('Y-m-d H:i:s') . "\n" .
+            "Note: " . $this->note . "\n" .
+            "Visiteur ID: " . $this->id_visiteur . "\n" .
+            "Visite ID: " . $this->id_visite . "\n";
+    }
+    // public function getCommentaire(): array
+    // {
+    //     return [
+    //         'id_commentaire' => $this->id_commentaire,
+    //         'contenu_commentaire' => $this->contenu_commentaire,
+    //         'date_commentaire' => $this->date_commentaire->format('Y-m-d H:i:s'),
+    //         'note' => $this->note,
+    //         'id_visiteur' => $this->id_visiteur,
+    //         'id_visite' => $this->id_visite
+    //     ];
+    // }
+    // public function setCommentaire(array $data): void
+    // {
+    //     if (isset($data['id_commentaire'])) {
+    //         $this->setIdCommentaire($data['id_commentaire']);
+    //     }
+    //     if (isset($data['contenu_commentaire'])) {
+    //         $this->setContenuCommentaire($data['contenu_commentaire']);
+    //     }
+    //     if (isset($data['date_commentaire'])) {
+    //         $this->setDateCommentaire($data['date_commentaire']);
+    //     }
+    //     if (isset($data['note'])) {
+    //         $this->setNote($data['note']);
+    //     }
+    //     if (isset($data['id_visiteur'])) {
+    //         $this->setIdVisiteur($data['id_visiteur']);
+    //     }
+    //     if (isset($data['id_visite'])) {
+    //         $this->setIdVisite($data['id_visite']);
+    //     }
+    // }
+    public function ajouterCommentaire(): bool
+    {
+        $conn = (new Connexion())->connect();
+        $sql = "INSERT INTO commentaires (texte, date_commentaire, note, id_utilisateur, id_visite) VALUES (:contenu_commentaire, :date_commentaire, :note, :id_visiteur, :id_visite)";
+        try {
+            $stmt = $conn->prepare($sql);
+        } catch (Exception $e) {
+            return false;
+        }
+        $stmt->bindParam(':contenu_commentaire', $this->contenu_commentaire);
+        $stmt->bindValue(':date_commentaire', $this->date_commentaire->format('Y-m-d H:i:s'));
+        $stmt->bindParam(':note', $this->note);
+        $stmt->bindParam(':id_visiteur', $this->id_visiteur);
+        $stmt->bindParam(':id_visite', $this->id_visite);
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getCommentaire(int $idCommentaire)
+    {
+        $conn = (new Connexion())->connect();
+        $sql = "SELECT * FROM commentaires WHERE id_commentaire = :id_commentaire";
+        try {
+            $stmt = $conn->prepare($sql);
+        } catch (Exception $e) {
+            return false;
+        }
+        $stmt->bindParam(':id_commentaire', $idCommentaire);
+        if ($stmt->execute()) {
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            if (
+                !empty($result)  &&
+                $this->setIdCommentaire($result['id_commentaire']) &&
+                $this->setContenuCommentaire($result['texte']) &&
+                $this->setNote($result['note']) &&
+                $this->setIdVisiteur($result['id_utilisateur']) &&
+                $this->setIdVisite($result['id_visite'])
+            )
+                return $this;
+        } else {
+            return false;
+        }
+    }
+    static function getAllCommentaires()
+    {
+        $conn = (new Connexion())->connect();
+        $sql = "SELECT * FROM commentaires  ";
+        try {
+            $stmt = $conn->prepare($sql);
+        } catch (Exception $e) {
+            return false;
+        }
+        if ($stmt->execute()) {
+            $resultats = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $allCommantaire = [];
+            // if (
+            //     !empty($result)
+            // ) {
+                $commentaire = new Commentaire();
+
+                foreach ($resultats as $result) {
+                    $commentaire->setIdCommentaire($result['id_commentaire']);
+                    $commentaire->setContenuCommentaire($result['texte']);
+                    $commentaire->setNote($result['note']);
+                    $commentaire->setIdVisiteur($result['id_utilisateur']);
+                    $commentaire->setIdVisite($result['id_visite']);
+                    $allCommantaire[] = $commentaire;
+                }
+        // }
+            return $allCommantaire;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+$comm = Commentaire::getAllCommentaires();
+print_r($comm);
---
+++ POO/habitas.php
@@ -0,0 +1,185 @@
+<?php
+class habitat
+{
+    private int $id_habitat;
+    private string $nom_habitat;
+    private string $description_habitat;
+    private string $zone_zoo;
+    private string $type_climat;
+    public function __construct() {}
+    public function getIdHabitat(): int
+    {
+        return $this->id_habitat;
+    }
+    public function getNomHabitat(): string
+    {
+        return $this->nom_habitat;
+    }
+    public function getDescriptionHabitat(): string
+    {
+        return $this->description_habitat;
+    }
+    public function getZoneZoo(): string
+    {
+        return $this->zone_zoo;
+    }
+    public function getTypeClimat(): string
+    {
+        return $this->type_climat;
+    }
+      public function setNomHabitat(string $nom_habitat)
+    {
+        $regix = "/^[a-zA-Z\s'-]{2,50}$/";
+        if (preg_match($regix, $nom_habitat)) {
+            $this->nom_habitat = $nom_habitat;
+            return true;
+        }
+        return false;
+    }
+    public function setDescriptionHabitat(string $description_habitat)
+    {
+        if (strlen($description_habitat) >= 10 && strlen($description_habitat) <= 500) {
+            $this->description_habitat = $description_habitat;
+            return true;
+        }
+        return false;
+    }
+    public function setZoneZoo(string $zone_zoo)
+    {
+        $regix = "/^[a-zA-Z\s'-]{2,50}$/";
+        if (preg_match($regix, $zone_zoo)) {
+            $this->zone_zoo = $zone_zoo;
+            return true;
+        }
+        return false;
+    }
+    public function setTypeClimat(string $type_climat)
+    {
+        $regix = "/^[a-zA-Z\s'-]{2,50}$/";
+        if (preg_match($regix, $type_climat)) {
+            $this->type_climat = $type_climat;
+            return true;
+        }
+        return false;
+    }
+
+    public function setIdHabitat(int $id_habitat)
+    {
+        if ($id_habitat > 0) {
+            $this->id_habitat = $id_habitat;
+            return true;
+        }
+        return false;
+    }
+    public function __toString()
+    {
+        return " id_habitat :" . $this->id_habitat . " nom_habitat :" . $this->nom_habitat . " description_habitat :" . $this->description_habitat . " zone_zoo :" . $this->zone_zoo . " type_climat :" . $this->type_climat;
+    }
+    public function ajouter_habitat(): bool
+    {
+        $conn = (new Connexion())->connect();
+        $sql = "INSERT INTO habitats (nom_habitat, description_habitat, zone_zoo, type_climat) VALUES (:nom_habitat, :description_habitat, :zone_zoo, :type_climat)";
+        try {
+            $stmt = $conn->prepare($sql);
+        } catch (Exception $e) {
+            return false;
+        }
+        $stmt->bindParam(':nom_habitat', $this->nom_habitat);
+        $stmt->bindParam(':description_habitat', $this->description_habitat);
+        $stmt->bindParam(':zone_zoo', $this->zone_zoo);
+        $stmt->bindParam(':type_climat', $this->type_climat);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function supprimer_habitat(int $id_habitat): bool
+    {
+        $conn = (new Connexion())->connect();
+        $sql = "DELETE FROM habitats WHERE id_habitat = :id_habitat";
+        try {
+            $stmt = $conn->prepare($sql);
+        } catch (Exception $e) {
+            return false;
+        }
+        if ($stmt->execute(["id_habitat" => $id_habitat])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function modifier_habitat(): bool
+    {
+        $conn = (new Connexion())->connect();
+        $sql = "UPDATE habitats SET nom_habitat = :nom_habitat, description_habitat = :description_habitat, zone_zoo = :zone_zoo, type_climat = :type_climat WHERE id_habitat = :id_habitat";
+        try {
+            $stmt = $conn->prepare($sql);
+        } catch 
+        (Exception ) {
+            return false;
+        }
+        $stmt->bindParam(':id_habitat', $this->id_habitat);
+        $stmt->bindParam(':nom_habitat', $this->nom_habitat);
+        $stmt->bindParam(':description_habitat', $this->description_habitat);
+        $stmt->bindParam(':zone_zoo', $this->zone_zoo);
+        $stmt->bindParam(':type_climat', $this->type_climat);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function getHabitat(): array|bool
+    {
+        $conn = (new Connexion())->connect();
+        $sql = "SELECT * FROM habitats WHERE id_habitat = :id_habitat";
+        try 
+        {
+            $stmt = $conn->prepare($sql);
+        } 
+        catch (Exception $e) {
+            return false;
+        }
+        $stmt->bindParam(':id_habitat', $this->id_habitat);
+        $stmt->execute();
+        $habitat = $stmt->fetch(pdo::FETCH_ASSOC);
+        if ($habitat) {
+            return $habitat;
+        } else {
+            return false;
+        }
+    }
+
+    public static function getAllHabitats(): array|bool
+    {
+        $conn = (new Connexion())->connect();
+        $sql = "SELECT * FROM habitats";
+        $allHabitats = [];
+
+        try {
+            $stmt = $conn->query($sql);
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            foreach ($results as $row) {
+                $h = new Habitat();
+                $h->setIdHabitat($row['id_habitat']);
+                $h->setNomHabitat($row['nom_habitat']);
+                $h->setDescriptionHabitat($row['description_habitat']);
+                $h->setZoneZoo($row['zone_zoo']);
+                $h->setTypeClimat($row['type_climat']);
+                $allHabitats[] = $h;
+            }
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+
+        return $allHabitats;
+    }
+
+
+
+}
